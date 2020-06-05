317b2a153e1fcbde3c6b198b8ae380b8b9b4b4c2
HafizZiq <hafizuddinismail552@gmail.com>
2020-06-05 17:08:37 +0800

    .
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -1,6 +1,6 @@
 """Make / Download Telegram Sticker Packs without installing Third Party applications
 Available Commands:
-.rm [Optional Emoji]
+.kangsticker [Optional Emoji]
 .packinfo
 .getsticker"""
 from telethon import events
@@ -29,7 +29,7 @@ from telethon.tl.types import (
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="rm"))
+@borg.on(admin_cmd(pattern="kangsticker ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -44,8 +44,8 @@ async def _(event):
 
     me = borg.me
     userid = borg.uid
-    packname = f"kang shit"
-    packshortname = f"Id_866307318"  # format: Uni_Borg_userid
+    packname = f"{userid}'s @UniBorg Pack"
+    packshortname = f"Uni_Borg_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
@@ -55,7 +55,7 @@ async def _(event):
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{userid}'s @AnimatedStickersGroup"
-        packshortname = f"Id_866307318_as"  # format: Uni_Borg_userid
+        packshortname = f"Uni_Borg_{userid}_as"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
